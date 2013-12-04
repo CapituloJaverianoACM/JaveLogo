fb28c4af59793d6acdce0db1ea176365b2b55eaa
simon0191@gmail.com <simon0191@gmail.com>
2013-12-04 16:21:52 -0500

    Initial commit

---
+++ gen/org/jfedor/nxtremotecontrol/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.jfedor.nxtremotecontrol;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/jfedor/nxtremotecontrol/model/NXTInstruction.java
@@ -0,0 +1,5 @@
+package org.jfedor.nxtremotecontrol.model;
+
+public class NXTInstruction {
+	
+}
